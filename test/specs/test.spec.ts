f3a0c61cc4748117cef0cd58377881308e6708d5
Naufal A <spealdm6@gmail.com>
2024-08-20 21:26:13 +0700

    wip: search and open detail article

--- test/specs/test.spec.ts
+++ test/specs/test.spec.ts
@@ -1,7 +1,34 @@
 import articlePage from "../pageobjects/article.page";
 
-describe("My Login application", () => {
-  it("[key: TD-T2160] User cek button back pada halaman Ringkasan Order", async () => {
+describe("Module Article", () => {
+  it("[key: TC-T1] User dapat mencari dan mengakses detail artikel", async () => {
     await articlePage.open();
+
+    const searchData = await $("//li[2]/a/div[2]/div/h2").getText();
+    console.log(searchData);
+
+    await $(`[name="searchArticle"]`).setValue(searchData);
+
+    await browser.execute(() => {
+      const submitButton = document.querySelector('input[type="submit"]');
+      if (submitButton) {
+        (submitButton as HTMLElement).click();
+      }
+    });
+
+    await browser.pause(2000);
+
+    const shownData = await $("//li[1]/a/div[2]/div/h2").getText();
+    console.log(shownData);
+
+    await expect(shownData).toBe(searchData);
+
+    await $(`//h2[normalize-space(text())='${shownData}']`).click();
+
+    await browser.pause(2000);
+
+    const articleTitle = await $("//section/h1").getText();
+
+    await expect(articleTitle).toBe(searchData);
   });
 });
